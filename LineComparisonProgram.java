acab0771126e8e55a835f62e6813d4291bc293d2
shital gawade <shitalgawade131@gmail.com>
2023-05-16 15:59:22 +0530

    welcome to program

---
+++ LineComparisonProgram.java
@@ -0,0 +1,10 @@
+package linecomparison;
+
+public class LineComparisonProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" Welcome to Line Comparison Computation Program");
+
+    }
+}
